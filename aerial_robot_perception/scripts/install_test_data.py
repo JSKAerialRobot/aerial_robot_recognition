73227173b19096e8fc30dc746625f0d6e8173377
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-04-26 20:03:22 +0900

    update the URL for testdata (rosbag)

--- aerial_robot_perception/scripts/install_test_data.py
+++ aerial_robot_perception/scripts/install_test_data.py
@@ -9,7 +9,7 @@ def main():
     download_data(
         pkg_name=PKG,
         path='test/data/2018-06-25-21-21-10_uav_with_red_object.bag',
-        url='https://drive.google.com/uc?id=1chQSj09vqDXKY3niRJpXEDCfZfqCbLf6',
+        url='https://drive.google.com/uc?id=1XyIJPSIP5Q97uBvZ6Q5hz9b-_0zGCH79',
         md5='54a46cf71924aabbe49bcc32d8c14c0a',
         extract=False,
     )
